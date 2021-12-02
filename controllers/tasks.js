d2596b026ea93a0b986bf8afebf97b3279267295
Ganiyat-dev <ganiyatolajumoke.go@gmail.com>
2021-12-02 10:12:41 +0100

    updated delete task controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -19,8 +19,6 @@ const createTask = async (req, res) => {
     }
 }
 
-
-
 const getTask = async (req, res) => {
     try {
         const { id: taskID } = req.params
@@ -28,7 +26,7 @@ const getTask = async (req, res) => {
         if (!task) {
             return res.status(404).json({ msg: `No task with id: ${taskID}` })
         }
-        res.status(200).json({ task })
+        res.status(200).json({ msg: task })
     } catch (error) {
         res.status(500).json({msg: error })
     }
@@ -37,8 +35,17 @@ const getTask = async (req, res) => {
 const updateTask = (req, res) => {
     res.send("update task")
 }
-const deleteTask = (req, res) => {
-    res.send("delete task")
+const deleteTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOneAndDelete({ _id: taskID })
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` })
+        }
+        res.status(200).json({ task: null, status: 'success' })
+    } catch (error) {
+        res.status(500).json({msg: error })
+    }
 }
 
 module.exports = {
